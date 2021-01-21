bb02b57ef5b2844ddca9751ba23f379bd6409678
Abraham Ezequiel Chalave <abraham.chalave@outlook.com>
2021-01-20 21:37:34 -0300

    Métodos de recuperación y de verificación agregados en EmailController

--- billetera_backend/billetera_backend/Controllers/EmailController.cs
+++ billetera_backend/billetera_backend/Controllers/EmailController.cs
@@ -4,21 +4,23 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Send_Mail_Webapi.Models;
+//using Send_Mail_Webapi.Models;
 using System.Net.Mail;
+using billetera_backend.Models;
 
 namespace billetera_backend.Controllers
 {
     public class EmailController : ApiController
     {
-        public IHttpActionResult sendmail(EmailClass ec)
+        [HttpPost]
+        public IHttpActionResult SendMailTo(EmailClass ec)
         {
             string subject = ec.subject;
             string to = ec.to;
             string body = ec.body;
 
             MailMessage mm = new MailMessage();
-            mm.From = new MailAddress("CORREO@ELECTRONICO.COM");
+            mm.From = new MailAddress("soporte.m.clip@gmail.com");
             mm.To.Add(to);
             mm.Subject = subject;
             mm.Body = body;
@@ -28,10 +30,35 @@ namespace billetera_backend.Controllers
             smtp.UseDefaultCredentials = true;
             smtp.Port = 587;
             smtp.EnableSsl = true;
-            smtp.Credentials = new System.Net.NetworkCredential("money@clip.com", "password");
+            smtp.Credentials = new NetworkCredential("soporte.m.clip@gmail.com", "g1ADw3aCDeHm");
             smtp.Send(mm);
 
             return Ok();
         }
+
+        public void Recuperar(string destinatario)
+        {
+            EmailClass mailBase = new EmailClass
+            {
+                to = destinatario,
+                subject = "Recuperación de cuenta/contraseña",
+                body = "Cuenta recuperada"
+            };
+
+            SendMailTo(mailBase);
+        }
+
+        public void Verificar(string destinatario)
+        {
+            EmailClass mailBase = new EmailClass
+            {
+                to = destinatario,
+                subject = "Verificación de cuenta",
+                body = "Verifique su cuenta"
+            };
+
+            SendMailTo(mailBase);
+        }
+
     }
 }
--- billetera_backend/billetera_backend/Models/EmailClass.cs
+++ billetera_backend/billetera_backend/Models/EmailClass.cs
@@ -7,9 +7,8 @@ namespace billetera_backend.Models
 {
     public class EmailClass
     {
-        public string to { get; set; };
-        public string subject { get; set; };
-
-        public string body { get; set; };
+        public string to { get; set; }
+        public string subject { get; set; }
+        public string body { get; set; }
     }
 }
